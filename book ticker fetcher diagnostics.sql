4528ed229cc28ffda30b4587a6861a45a7e18d2d
frenchgrandpa <pepijn.pinckers@hotmail.nl>
2022-09-26 22:34:08 +0200

    s

---
+++ book ticker fetcher diagnostics.sql
@@ -0,0 +1,36 @@
+SELECT datname, pid, state, query, age(clock_timestamp(), query_start) AS age
+FROM pg_stat_activity
+WHERE state <> 'idle'
+  AND query NOT LIKE '% FROM pg_stat_activity %'
+ORDER BY age;
+
+SELECT
+    schema_name,
+    relname,
+    pg_size_pretty(table_size) AS size,
+    table_size
+
+FROM (
+    SELECT
+    pg_catalog.pg_namespace.nspname           AS schema_name,
+    relname,
+    pg_relation_size(pg_catalog.pg_class.oid) AS table_size
+
+    FROM pg_catalog.pg_class
+    JOIN pg_catalog.pg_namespace ON relnamespace = pg_catalog.pg_namespace.oid
+    ) t
+WHERE schema_name NOT LIKE 'pg_%'
+ORDER BY table_size DESC;
+
+select schema_name
+from information_schema.schemata;
+
+GRANT SELECT ON ALL TABLES IN SCHEMA timescaledb_experimental TO grafana;
+ALTER DEFAULT PRIVILEGES IN SCHEMA timescaledb_experimental
+    GRANT SELECT ON TABLES TO grafana;
+
+
+
+VACUUM FULL Analyze liquidity_logs;
+
+
